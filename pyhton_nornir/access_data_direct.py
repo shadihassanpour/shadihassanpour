e4ac79cd24d401fd76a797b8dbaee04eca5db7a1
shadihassanpour <155784730+shadihassanpour@users.noreply.github.com>
2024-01-14 04:56:46 +0330

    Update access_data_direct.py

--- pyhton_nornir/access_data_direct.py
+++ pyhton_nornir/access_data_direct.py
@@ -1,25 +1,17 @@
 from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
-print(nr.inventory.hosts)
-for group in nr.inventory.groups:
-  print(nr.inventory.groups[group].data['community'])
-for host in nr.inventory.hosts:
-  print(nr.inventory.hosts[host].data['community'])
+def acc_data(task):
+    print(task.host['community'])
+
+results = nr.run(task=acc_data)
+print_result(results)
 
 
-#comment:( we have host.yaml,group.yaml,default.yaml, config.yaml)
-# when u want config, u need date for example : as num ?, neighbor ?, other data.
-# save data in inventory and access this data.
-# prioritize: first: host ,second: group , third:default .
-# for add data u just write data at the same level hostname:# as below :
-        hostnam:  "192.168.1.1"
-           username: "zitel"
-           password: "zitelzitel"
-        data:
-           community: zitel-co.ir
-           router_id: 1.1.1.1
-           neighbor_id: 192.168.1.2
-#for access inventory just print (nr.inventory)
-#for access list hosts in inventory print(nr.inventory.hosts)
-# for access list groups in inventory print(nr.inventory.groups)
+
+
+#comment
+#  task : any task 
+#this task just print community in host part in hosts.yaml or groups.yaml or defaults.yaml (prioritize: first: host ,second: group , third:default )
+# config.yaml ( contains of : plugin ,host_file,group_file,default_file)
