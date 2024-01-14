c74f4190d96ce6779b58aa8a24331f8b7c5a9ffd
shadihassanpour <155784730+shadihassanpour@users.noreply.github.com>
2024-01-14 07:04:45 +0330

    Update 3.nornir_scrapli_send_config_1.py

--- pyhton_nornir/3.nornir_scrapli_send_config_1.py
+++ pyhton_nornir/3.nornir_scrapli_send_config_1.py
@@ -13,6 +13,8 @@ for host in nr.inventory.hosts:
 # we already khonw that we need data fo config devices ( access with inventory)
 # for send config u must import send_config in nornir_scrapli.tasks 
 # def send_config(task: Task,config: str,dru_run:optional(bool))
-# dry_run just check that config run on devices and does not apply.
-
-
+# dry_run just ,default= none ,check that config run on devices and apply it,if dry_run= true config apply.
+# in header u must import send_config from nornir_scrapli.tasks
+# function fstring:convert data to value 
+#The community is a type of shared password between the SNMP management station and the device, which is used to authenticate the SNMP management station. Communities are only defined in SNMPv1 and v2 because SNMP v3 works with users instead of communities.
+#
